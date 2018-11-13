815f26abc4ffc677f4cce0d0d6aa79d6d886e56e
balo11044 <balo11044@gmail.com>
2018-11-13 20:42:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/webhook', function (req, res) { // Phần sử lý tin nhắn của n
 // Đây là function dùng api của facebook để gửi tin nhắn
 function sendMessage(senderId, message) {
     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
+        url: 'https://graph.facebook.com/v3.2/me/messages',
         qs: {
             access_token: config.PAGE_ACCESS_TOKEN,
         },
