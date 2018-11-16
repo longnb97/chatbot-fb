d6aaccdb237a229c2d7c006a2408e9dd40c057f9
balo11044 <balo11044@gmail.com>
2018-11-16 15:11:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const APP_SECRET = 'f8fe27169d1328156aea4c22b8634de3';
 const VALIDATION_TOKEN = 'tokenn';
 const PAGE_ACCESS_TOKEN = 'EAAEwKzAEQqwBAFOrrsygQE1OIcZBP1F4okVR2WeJiDXt82a6AmQ4aRp11UH42gZAlq5DKzLoBGqOzZBfsm9iBmGi65DvFGcHUuGVN5Xr3KtYaF1eZCG7dWdfQdk8H9zyVZCgA5arkACUPr8ZAIFJODqIZBFHTwZAE9b53s6Wn5lEdgZDZD';
 
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
+bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
 var request = require("request");
 
 app.get('/', (req, res) => {
@@ -22,7 +24,7 @@ app.get('/webhook', function (req, res) { // Đây là path để validate tooke
     res.send('Error, wrong validation token');
 });
 
-app.post('/webhook', function (req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
+app.post('/webhook/', function (req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
     var entries = req.body.entry;
     for (var entry of entries) {
         var messaging = entry.messaging;
@@ -43,7 +45,10 @@ app.post('/webhook', function (req, res) { // Phần sử lý tin nhắn của n
 // Đây là function dùng api của facebook để gửi tin nhắn
 function sendMessage(senderId, message) {
     request({
-        url: `https://graph.facebook.com/v2.6/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: PAGE_ACCESS_TOKEN,
+        },
         method: 'POST',
         json: {
             recipient: {
@@ -57,8 +62,7 @@ function sendMessage(senderId, message) {
 }
 
 app.set('port', process.env.PORT || 5000);
-app.set('ip', process.env.IP || "0.0.0.0");
 
-server.listen(app.get('port'), app.get('ip'), function () {
-    console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log("Chat bot server listening at ", app.get('port'));
 });
